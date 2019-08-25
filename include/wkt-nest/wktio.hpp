be00405eaf520ad714e61db34390d90388d082fa
hrobeers <2812522+hrobeers@users.noreply.github.com>
2019-08-25 12:59:18 +0200

    Polygon multiplier support on stdin

--- include/wkt-nest/wktio.hpp
+++ include/wkt-nest/wktio.hpp
@@ -14,12 +14,33 @@ namespace wktnest
   std::optional<box_t> read_box(std::istream &in);
   std::optional<polygon_t> read_polygon(std::istream &in);
 
+  inline size_t read_multiplier(std::istream &in) {
+    size_t multiplier;
+    while (in)
+      switch (in.peek()) {
+      case ' ':
+        in.get();
+        break;
+      case 'x':
+        in.get();
+        in >> multiplier;
+        std::cerr << "multiplier: " << multiplier << std::endl;
+        return multiplier;
+      default:
+        return 1;
+      }
+    return 1;
+  }
+
   inline
   std::vector<polygon_t> read_polygons(std::istream &in) {
     std::vector<polygon_t> polygons;
     while (in)
-      if (auto p = read_polygon(in))
-        polygons.push_back(*p);
+      if (auto p = read_polygon(in)) {
+        size_t m = read_multiplier(in);
+        for (size_t i=0; i<m; i++)
+          polygons.push_back(*p);
+      }
     return polygons;
   }
 }
