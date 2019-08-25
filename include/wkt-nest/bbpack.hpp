040c602aa643d19be66923912f63e14b52555147
hrobeers <2812522+hrobeers@users.noreply.github.com>
2019-08-25 12:15:54 +0200

    bbpack: item class for polygon manipulations

--- include/wkt-nest/bbpack.hpp
+++ include/wkt-nest/bbpack.hpp
@@ -5,31 +5,56 @@
 
 #include <vector>
 #include <map>
+#include <set>
 
 #include "wkt-nest/geometry.hpp"
 
 namespace wktnest {
   namespace bbpack {
+    struct transform_t { /* TODO */ };
+    class item_t {
+      static std::set<const polygon_t*> s_placed;
+      const polygon_t* _source;
+
+      polygon_t _polygon;
+      box_t _bbox;
+      transform_t _transform;
+
+    public:
+      item_t(const polygon_t& p);
+
+      const polygon_t* polygon() const { return &_polygon; }
+      const box_t* bbox() const { return &_bbox; }
+      const transform_t* transform() const { return &_transform; }
+      bool placed() const { return s_placed.count(_source); }
+
+      void apply_transform(transform_t &t) { /* TODO */ }
+    };
     struct node_t {
       box_t box;
       bool used = false;
       node_t* up = nullptr;
       node_t* right = nullptr;
     };
-    struct state {
+    struct state_t {
       box_t bin;
+      std::vector<item_t> items;
+      std::map<const item_t*, size_t> item_to_bin_idx;
+
+      // TODO below still needed?
+
       // list of nodes per bin
       //std::vector<std::vector<node_t>> nodes;
       // single bin of nodes TODO switch to multiple bins
       std::vector<node_t> nodes;
       // fit per bbox
-      std::map<const box_t*,node_t*> fits;
+      std::map<const item_t*,node_t*> fits;
     };
 
-    state init(box_t& bin);
-    std::vector<box_t> fit(state& s, std::vector<polygon_t> polygons);
-    node_t* find_node(state& s, node_t* root, box_t& bbox);
-    node_t* split_node(state& s, node_t* n, box_t& bbox);
+    state_t init(const box_t& bin);
+    std::vector<box_t> fit(state_t& s, const std::vector<polygon_t>& polygons);
+    node_t* find_node(state_t& s, node_t* root, item_t* item);
+    node_t* split_node(state_t& s, node_t* n, item_t* item);
   }
 }
 
--- src/wkt-nest/bbpack.cpp
+++ src/wkt-nest/bbpack.cpp
@@ -15,49 +15,52 @@ namespace {
     double w;
     double h;
   };
-  dim_t dims(const box_t& b) {
-    double min_x = bg::get<bg::min_corner, 0>(b);
-    double min_y = bg::get<bg::min_corner, 1>(b);
-    double max_x = bg::get<bg::max_corner, 0>(b);
-    double max_y = bg::get<bg::max_corner, 1>(b);
+  dim_t dims(const box_t* b) {
+    double min_x = bg::get<bg::min_corner, 0>(*b);
+    double min_y = bg::get<bg::min_corner, 1>(*b);
+    double max_x = bg::get<bg::max_corner, 0>(*b);
+    double max_y = bg::get<bg::max_corner, 1>(*b);
     return { max_x-min_x, max_y-min_y };
   }
 }
 
-state bbpack::init(box_t& bin) {
+
+bbpack::item_t::item_t(const polygon_t& p) {
+  _source = &p;
+  // TODO apply transform identity instead of code below
+  // TODO transform to origin
+  _polygon = p;
+  bg::envelope(_polygon,_bbox);
+}
+
+
+state_t bbpack::init(const box_t& bin) {
   return { bin };
 }
 
-std::vector<box_t> bbpack::fit(state& s, std::vector<polygon_t> polygons) {
+std::vector<box_t> bbpack::fit(state_t& s, const std::vector<polygon_t>& polygons) {
 
-  // Create bbox vector TODO: rotation (cmd flag)
-  std::vector<box_t> bboxes;
-  bboxes.resize(polygons.size());
-  std::transform(polygons.cbegin(), polygons.cend(), bboxes.begin(),
-                 [](const polygon_t& p) -> box_t {
-                   box_t b;
-                   bg::envelope(p,b);
-                   return b;
-                 });
+  for (auto p : polygons)
+    s.items.push_back(item_t(p));
 
   // construct root
   s.nodes.push_back({s.bin});
   node_t* root = &s.nodes.back();
 
-  for (box_t& bbox : bboxes)
-    if (auto node = find_node(s, root, bbox)) // TODO else pack in other bin
-      s.fits[&bbox] = split_node(s, node, bbox);
+  for (item_t& item : s.items)
+    if (auto node = find_node(s, root, &item)) // TODO else pack in other bin
+      s.fits[&item] = split_node(s, node, &item);
 
   // Create fitted box vector
   std::vector<box_t> fboxes;
-  fboxes.resize(bboxes.size());
-  std::transform(bboxes.cbegin(), bboxes.cend(), fboxes.begin(),
-                 [&s](const box_t& bbox) -> box_t {
+  fboxes.resize(s.items.size());
+  std::transform(s.items.cbegin(), s.items.cend(), fboxes.begin(),
+                 [&s](const item_t& item) -> box_t {
                    // TODO filter first on fit
-                   node_t* n = s.fits[&bbox];
+                   node_t* n = s.fits[&item];
                    //if (!n) return box_t();
-                   point_t min = bbox.min_corner();
-                   point_t max = bbox.max_corner();
+                   point_t min = item.bbox()->min_corner();
+                   point_t max = item.bbox()->max_corner();
                    bg::add_point(min, n->box.min_corner());
                    bg::add_point(max, n->box.min_corner());
                    return box_t(min, max);
@@ -66,18 +69,18 @@ std::vector<box_t> bbpack::fit(state& s, std::vector<polygon_t> polygons) {
   return fboxes;
 }
 
-node_t* bbpack::find_node(state& s, node_t* root, box_t& bbox) {
+node_t* bbpack::find_node(state_t& s, node_t* root, item_t* item) {
 
   if (root->used) {
     node_t* up = root->up;
     node_t* right = root->right;
-    if (auto node = find_node(s, right, bbox))
+    if (auto node = find_node(s, right, item))
       return node;
-    return find_node(s, up, bbox);
+    return find_node(s, up, item);
   }
 
-  auto rtdims = dims(root->box);
-  auto bbdims = dims(bbox);
+  auto rtdims = dims(&root->box);
+  auto bbdims = dims(item->bbox());
 
   if ((bbdims.w <= rtdims.w) && (bbdims.h <= rtdims.h))
     return root;
@@ -85,9 +88,9 @@ node_t* bbpack::find_node(state& s, node_t* root, box_t& bbox) {
   return nullptr;
 }
 
-node_t* bbpack::split_node(state& s, node_t* n, box_t& bbox) {
+node_t* bbpack::split_node(state_t& s, node_t* n, item_t* item) {
 
-  s.fits[&bbox] = n;
+  s.fits[item] = n;
   n->used = true;
 
   double n_min_x = bg::get<bg::min_corner, 0>(n->box);
@@ -95,7 +98,7 @@ node_t* bbpack::split_node(state& s, node_t* n, box_t& bbox) {
   double n_max_x = bg::get<bg::max_corner, 0>(n->box);
   double n_max_y = bg::get<bg::max_corner, 1>(n->box);
 
-  auto bbdims = dims(bbox);
+  auto bbdims = dims(item->bbox());
 
   // node up
   box_t up = {{n_min_x, n_min_y + bbdims.h}, {n_max_x, n_max_y}};
